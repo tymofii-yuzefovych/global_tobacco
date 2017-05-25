d4bbf10c9895b6630a7ec38204a9ad0e9a30c20f
Tymofii Yuzefovych <tymofii.yuzefovych@gmail.com>
2017-05-25 16:02:27 +0300

    create new branch and create build system

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,47 @@ gulp.task('sass', function() {
 
 });
 
-gulp.task('default', ['sass'], function() {
+
+// Minify CSS - Minifies CSS
+gulp.task('minify-css', function() {
+    gulp.src(['css/style.css', '!css/style.min.css'])
+        .pipe(cleanCSS({ debug: true }, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css/'));
+});
+
+// Minify JS - Minifies JS
+gulp.task('minify-js', function(cb) {
+    pump([
+            gulp.src(['js/**/*.js', '!js/**/*.min.js']),
+            uglify(),
+            rename({ suffix: '.min' }),
+            gulp.dest('js/')
+        ],
+        cb
+    );
+});
+
+// Htmlhint - Validate HTML
+gulp.task('htmlhint', function() {
+    gulp.src('*.html')
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+        .pipe(htmlhint.failReporter({ suppress: true }))
+});
+
+// This handles watching and running tasks
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('css/style.css', ['minify-css']);
+    gulp.watch('js/**/*.js', ['minify-js']);
+    gulp.watch('*.html', ['htmlhint']);
+});
+
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'htmlhint', 'watch'], function() {
 
     browserSync.init({
         server: {
@@ -36,58 +76,6 @@ gulp.task('default', ['sass'], function() {
     });
 
     gulp.watch(Files.scss_all, ['sass']);
-    gulp.watch(Files.html, browserSync.reload);
+    gulp.watch(Files.html, browserSync.reload());
 
 });
-
-// // Minify CSS - Minifies CSS
-// gulp.task('minify-css', function() {
-//     gulp.src(['css/style.css', '!css/style.min.css'])
-//         .pipe(cleanCSS({ debug: true }, function(details) {
-//             console.log(details.name + ': ' + details.stats.originalSize);
-//             console.log(details.name + ': ' + details.stats.minifiedSize);
-//         }))
-//         .pipe(rename({ suffix: '.min' }))
-//         .pipe(gulp.dest('css/'));
-// });
-
-// // Minify JS - Minifies JS
-// gulp.task('minify-js', function(cb) {
-//     pump([
-//             gulp.src(['js/**/*.js', '!js/**/*.min.js']),
-//             uglify(),
-//             rename({ suffix: '.min' }),
-//             gulp.dest('js/')
-//         ],
-//         cb
-//     );
-// });
-
-// // Htmlhint - Validate HTML
-// gulp.task('htmlhint', function() {
-//     gulp.src('*.html')
-//         .pipe(htmlhint())
-//         .pipe(htmlhint.reporter())
-//         .pipe(htmlhint.failReporter({ suppress: true }))
-// });
-
-// // This handles watching and running tasks
-// gulp.task('watch', function() {
-//     gulp.watch('scss/**/*.scss', ['sass']);
-//     gulp.watch('css/style.css', ['minify-css']);
-//     gulp.watch('js/**/*.js', ['minify-js']);
-//     gulp.watch('*.html', ['htmlhint']);
-// });
-
-// gulp.task('default', ['sass', 'minify-css', 'minify-js', 'htmlhint', 'watch'], function() {
-
-//     browserSync.init({
-//         server: {
-//             baseDir: "./"
-//         }
-//     });
-
-//     gulp.watch(Files.scss_all, ['sass']);
-//     gulp.watch(Files.html, browserSync.stream());
-
-// });
